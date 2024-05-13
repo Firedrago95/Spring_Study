562a5bf296c658fc4b8a149bf7df7f428e02aced
YONGHWA\ekore <yonghwa95@naver.com>
2024-05-13 10:57:48 +0900

    Jdk 동적 프록시 필터적용

--- src/main/java/hello/proxy/config/v2_dynamicproxy/handler/LogTraceFilterHandler.java
+++ src/main/java/hello/proxy/config/v2_dynamicproxy/handler/LogTraceFilterHandler.java
@@ -2,24 +2,31 @@ package hello.proxy.config.v2_dynamicproxy.handler;
 
 import hello.proxy.trace.TraceStatus;
 import hello.proxy.trace.logtrace.LogTrace;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.PatternMatchUtils;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
-@Slf4j
-public class LogTraceBasicHandler implements InvocationHandler {
+public class LogTraceFilterHandler implements InvocationHandler {
 
     private final Object target;
     private final LogTrace logTrace;
 
-    public LogTraceBasicHandler(Object target, LogTrace logTrace) {
+    private final String[] patterns;
+
+    public LogTraceFilterHandler(Object target, LogTrace logTrace, String... patterns) {
         this.target = target;
         this.logTrace = logTrace;
+        this.patterns = patterns;
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        // 입력받은 패턴이름과 메서드명 일치하지 않으면 바로 target 로직 실행
+        if (!PatternMatchUtils.simpleMatch(patterns, method.getName())) {
+            return method.invoke(target, args);
+        }
+
         TraceStatus status = null;
         try {
             String message = method.getDeclaringClass().getSimpleName() + "."
