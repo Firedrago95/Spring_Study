3dd8c216a4c96017a83f2aaddd867a4c78f2b6f1
YONGHWA\ekore <yonghwa95@naver.com>
2024-05-14 13:06:51 +0900

    프록시 팩토리 적용

---
+++ src/main/java/hello/proxy/config/v3_proxyfactory/advice/LogTraceAdvice.java
@@ -0,0 +1,34 @@
+package hello.proxy.config.v3_proxyfactory.advice;
+
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+@Slf4j
+public class LogTraceAdvice implements MethodInterceptor {
+
+    private final LogTrace logTrace;
+
+    public LogTraceAdvice(LogTrace logTrace) {
+        this.logTrace = logTrace;
+    }
+
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        TraceStatus status = null;
+        try {
+            String message = invocation.getClass().getName() + "." + invocation.getMethod().getName();
+            status = logTrace.begin(message);
+
+            Object result = invocation.proceed();
+
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
